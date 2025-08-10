1b32f13bd66ebac69360c18cdeb48cba158f6ed2
Soren Rasmussen <s.c.rasmussen@gmail.com>
2025-08-10 16:57:32 +0000

    Moving variable names to settings files

--- initialConditions/global.js
+++ initialConditions/global.js
@@ -2,4 +2,27 @@ export const settings = {
   lat: 38,
   lon: -37,
   zoom: 1.5,
+
+  variables: [
+      'n34pr',
+      'n34t',
+      'pr90',
+      'pr99',
+      't90',
+      't99',
+      'djf_t',
+      'djf_p',
+      'mam_t',
+      'mam_p',
+      'jja_t',
+      'jja_p',
+      'son_t',
+      'son_p',
+      'ann_t',
+      'ann_p',
+      'ann_snow',
+      'freezethaw'
+  ],
+  variables_trend: ['ptrend', 'ttrend'],
+
 };
