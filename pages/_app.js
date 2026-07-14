4e3ec85d7ccefd3096d8db9c5a6713be7f74486e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2026-07-14 10:49:14 -0600

    test fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from 'theme-ui'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
-import '@carbonplan/maps/mapbox.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import theme from '@carbonplan/theme'
 
 const App = ({ Component, pageProps }) => {
